7164c2857c026ef7a3fb192660d148ebe813bfa8
abondarenko <abondarenko@nota.tech>
2025-06-05 21:53:12 +0300

    П. 5-8

--- src/main/java/org/example/task2/CollectionsBoard.java
+++ src/main/java/org/example/task2/CollectionsBoard.java
@@ -1,9 +1,8 @@
 package org.example.task2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -11,9 +10,16 @@ public class CollectionsBoard {
     public static void collectionsRun() {
 
         //1. Найдите в списке целых чисел 3-е наибольшее число (пример: 5 2 10 9 4 3 10 1 13 => 10)
+
         Stream.of(5, 2, 10, 9, 4, 3, 10, 1, 13)
                 .sorted(Comparator.reverseOrder()).limit(3).skip(2)
                 .forEach(System.out::println);
+
+        System.out.println("||");
+
+        Stream.of(5, 2, 10, 9, 4, 3, 10, 1, 13)
+                .sorted(Comparator.reverseOrder())
+                .skip(2).findFirst().ifPresent(System.out::println);
 
         System.out.println("--------------");
 
@@ -60,6 +66,44 @@ public class CollectionsBoard {
         System.out.println("--------------");
 
         //6. Имеется строка с набором слов в нижнем регистре, разделенных пробелом. Постройте хеш-мапы,
-        // в которой будут хранится пары: слово - сколько раз оно встречается во входной строке
+        // в которой будут храниться пары: слово - сколько раз оно встречается во входной строке
+        String textSeven = "лишь многие известные личности лишь инициированные исключительно синтетически объективно инициированные рассмотрены соответствующими инстанциями";
+
+/*        Stream.of("лишь многие известные личности лишь инициированные исключительно синтетически объективно инициированные рассмотрены соответствующими инстанциями")
+                .flatMap(s -> Arrays.stream(s.split(" ")))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).
+                forEach((word, count) -> System.out.println(word + " : " + count));*/    //2 терминальных метода не уверена , что так правильно , хотя компилятор пропускает
+
+        Map<String, Long> wordCount = Stream.of(textSeven)
+                .flatMap(s -> Arrays.stream(s.split(" ")))
+                .collect(Collectors.groupingBy(
+                        Function.identity(),
+                        Collectors.counting()
+                ));
+
+        wordCount.forEach((word, count) -> System.out.println(word + " : " + count));
+
+        System.out.println("--------------");
+
+        //7. Отпечатайте в консоль строки из списка в порядке увеличения длины слова, если слова имеют одинаковую длины, то должен быть сохранен алфавитный порядок
+
+        Stream.of("Желание","Ржавый","Семнадцать","Рассвет","Печь","Девять","Добросердечный","Один","Аппп").
+                sorted(Comparator.comparingInt(String::length).thenComparing(Comparator.naturalOrder())).
+                forEach(System.out::println);
+
+
+        System.out.println("--------------");
+        //8. Имеется массив строк, в каждой из которых лежит набор из 5 слов, разделенных пробелом, найдите среди всех слов самое длинное,
+        // если таких слов несколько, получите любое из них
+
+        String[] strWords ={
+                "Один два три четыре пять",
+                "шесть семь восемь девять десять",
+                "ддвенадцать одиннадцать восемь девять десять"
+        };
+
+        Arrays.stream(strWords).
+                flatMap(s -> Arrays.stream(s.split(" ")))
+                .max(Comparator.comparing(String::length)).ifPresent(System.out::println);
     }
 }
